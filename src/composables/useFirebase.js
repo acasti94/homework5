e5ba815c5a942fba1c84645d6bcee6f2b7f9825e
Adelson Castillo <adelsonramon@gmail.com>
2024-10-02 21:23:38 -0500

    authentication updated

--- src/composables/useFirebase.js
+++ src/composables/useFirebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBORSJm91fRck5N85AiOBfqhQfZ22ioTlo",
   authDomain: "homework5-16b33.firebaseapp.com",
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: "1:688585966724:web:03979b0616639e2d69eed1"
 };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseApp = initializeApp(firebaseConfig);
